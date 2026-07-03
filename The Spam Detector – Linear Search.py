e15b071ad41ec1c2e380f80c2152e5e55aac9fd5
Pavitri <davepavitri@gmail.com>
2026-07-03 09:20:14 +0530

    Update The Spam Detector – Linear Search.py

--- The Spam Detector – Linear Search.py
+++ The Spam Detector – Linear Search.py
@@ -1,10 +1,10 @@
 """A cybersecurity intern at a startup is building a basic spam filter. Incoming emails are checked against a blacklist of known spam sender IDs. The blacklist is not sorted. Write a program to search whether a sender ID exists in the blacklist using the appropriate searching technique."""
 blacklist = ["spam123", "abc456", "fake789", "test111"]
-sender = input("Enter sender ID: ")
+id = input("Enter sender's ID: ")
 found = False
 
 for i in blacklist:
-    if i == sender:
+    if i == id:
         found = True
         break
 
